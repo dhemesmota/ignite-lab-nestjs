7b5c3395de4dd5dc0de680b1def50ae5a7c89b6a
Dhemes Mota <dhemes.mota@gmail.com>
2022-12-14 10:44:25 -0300

    feat: add structur and entities

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('shoud be able to send a notification', async () => {
+    const notificationsRepsository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepsository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notificationsRepsository.notifications).toHaveLength(1);
+    expect(notificationsRepsository.notifications[0]).toEqual(notification);
+  });
+});
